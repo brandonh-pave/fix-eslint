c5bc89556af19e39f0eb35fbfe3a1cb93c1c0761
Brandon Hsiao <brandon.hsiao@pave.com>
2023-11-03 12:46:44 -0700

    use esprima instead, cleanup, env var for frontend path

--- index.js
+++ index.js
@@ -1,48 +1,47 @@
 import { readFileSync, writeFileSync } from 'fs';
 import { promisify } from 'util';
 import { exec } from 'child_process';
-import { parse } from 'acorn';
+import { parse } from 'esprima';
 import { generate } from 'escodegen';
 
 const _exec = promisify(exec);
 
-function fixEslintConfig(ast) {
-  const fixErrorLiteral = (node) => {
-    if (node.type === 'Literal' && node.value === 'error') {
-      node.value = 'warn';
-      node.raw = "'warn'";
-    }
-  };
+process.chdir(process.env.FIX_ESLINT_FRONTEND_PATH)
 
+function changeErrorToWarn(node) {
+  if (node.type === 'Literal' && node.value === 'error') {
+    node.value = 'warn';
+    node.raw = "'warn'";
+  }
+}
+
+function fixEslintrc(ast) {
   const rules = ast.body[0].expression.right.properties
     .find(it => it.key.name === 'rules');
 
   for (const { value } of rules.value.properties) {
     if (value.type === 'ArrayExpression') {
-      fixErrorLiteral(value.elements[0]);
+      changeErrorToWarn(value.elements[0]);
     } else {
-      fixErrorLiteral(value);
+      changeErrorToWarn(value);
     }
   }
+
+  return ast;
 }
 
 async function main() {
-  process.chdir('/Users/brandonhsiao/frontend');
-
   const { stdout } = await _exec('ag --hidden -g .eslintrc.js');
   const filenames = stdout.split('\n').filter(Boolean);
 
   for (const f of filenames) {
-    console.log(`fixing ${f}`);
+    console.log(`Fixing ${f}`);
 
+    // hide it from git status, git diff, etc.
     await _exec(`git update-index --assume-unchanged ${f}`);
 
-    const contents = readFileSync(f, 'utf-8');
-    const astNode = parse(contents, { ecmaVersion:  2020 });
-    const ast = JSON.parse(JSON.stringify(astNode));
-
-    fixEslintConfig(ast);
-    writeFileSync(f, generate(ast));
+    // replace errors with warns
+    writeFileSync(f, generate(fixEslintrc(parse(readFileSync(f, 'utf8')))));
   }
 }
 
